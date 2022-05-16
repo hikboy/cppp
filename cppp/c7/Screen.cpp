650dd472ef7dd8575660e682f72a426bdc0ee369
ljb <hikboy@outlook.com>
2022-05-16 17:00:23 +0800

    add out of class inline member funtions

--- cppp/c7/Screen.cpp
+++ cppp/c7/Screen.cpp
@@ -1,2 +1,16 @@
 
 #include "Screen.h"
+
+
+inline Screen &Screen::move(pos r, pos c)
+{
+    pos row = r* width;
+    cursor = row + c;
+    return *this;
+}
+
+char Screen::get(pos ht, pos wd) const
+{
+    pos ros = r* width;
+    return contents[row+c];
+}
