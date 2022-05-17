54ebe8a6c305454c606bef96d89b5824d4b6190e
ljb <hikboy@outlook.com>
2022-05-17 14:29:11 +0800

    another static thing

--- cppp/c7/Account.h
+++ cppp/c7/Account.h
@@ -12,8 +12,19 @@ private:
     double amount;
     static double interestRate;
     static double initRate();
-    //static const double dper = 11.0;
+    //static const double dper = 11.0; //just for intergral
     static const int xper = 11;
     static constexpr int period = 30;
     double daily_tbl[period];
+};
+
+////////////////////////////////////////////////////////////////////////////////
+
+class Bar {
+public:
+
+private:
+    static Bar mem1;
+    Bar *mem2;
+    //Bar mem3;
 };
