2da824ed299c83c28cf7751ac2644c5d22069536
ljb <hikboy@outlook.com>
2022-05-26 10:01:39 +0800

    add new test for algorithm

--- cppp/c10/c10_1.cpp
+++ cppp/c10/c10_1.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <algorithm>
 #include <numeric>
+#include <list>
 
 using namespace std;
 
@@ -34,6 +35,48 @@ int main(void)
     const char* rouster1 = "hello";
     const char* rouster2 = "hello";
 
+    fill(vv.begin(), vv.end(), 0.0);
+    for(auto ivv = vv.begin(); ivv != vv.end(); ivv++)
+        cout << *ivv << endl;
+
+    fill_n(vv.begin(), vv.size(), 100);
+    for(auto ivv = vv.begin(); ivv != vv.end(); ivv++)
+        cout << *ivv << endl;
     //auto r2 = equal(rouster1.cbegin(), rouster1.cend(), rouster2.cbegain());
+
+    vector<int> vccr;
+    auto it = back_inserter(vccr);
+    *it = 55;
+    cout << "vccr size is " << vccr.size() << endl;
+
+    fill_n(back_inserter(vccr), 10, 11);
+    cout << "vccr size is " << vccr.size() << endl;
+
+    int a1[] = {0,1,2,3,4,5};
+    cout << "sizeof(a1) is " << sizeof(a1) << endl;
+    cout << "sizeof(*a1) is " << sizeof(*a1) << endl;
+
+    int a2[sizeof(a1)/sizeof(*a1)];
+
+    auto ret = copy(begin(a1), end(a1), begin(a2));
+    //cout << "copy ret is " << *ret << endl;
+    cout << "copy is " << (ret == end(a2)? "ok": "bad") << endl;
+
+    vector<int> hi {0,0,0,0,0,0,0,0,0,0,0,0};
+    replace(hi.begin(), hi.end(), 0, 55);
+    for(auto iii = hi.begin(); iii != hi.end(); iii++)
+        cout << *iii << ' ';
+    cout << endl;
+
+    vector<int> llo;
+    replace_copy(hi.begin(), hi.end(), back_inserter(llo), 55, 66);
+    for(auto iii = llo.begin(); iii != llo.end(); iii++)
+        cout << *iii << ' ';
+    cout << endl;
+
+    list<int> lst {1,2,3,4,5,6,7};
+    vector<int> lvec;
+    copy(lst.cbegin(), lst.cend(), back_inserter(lvec));
+
     return 0;
 }
---
+++ cppp/c10/c10_2.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <fstream>
+
+using namespace std;
+
+void elimDups(vector<string> &words)
+{
+    sort(words.begin(), words.end());
+
+    auto end_unique = unique(words.begin(), words.end());
+
+    words.erase(end_unique, words.end());
+
+}
+
+int main(void)
+{
+    ifstream s("../data/story.txt");
+    vector<string> vs;
+    string ts;
+    while(s >> ts)
+        vs.push_back(ts);
+
+    for(auto i = vs.begin(); i != vs.end(); i++)
+        cout << *i << endl;
+
+    return 0;
+}
