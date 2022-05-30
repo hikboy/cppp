2fecd069582556164cb4055b6df3a6451d78f3fa
ljb <hikboy@outlook.com>
2022-05-30 17:50:52 +0800

    add c11 for map and set

--- cppp/c11/c11_01.cpp
+++ cppp/c11/c11_01.cpp
@@ -1,11 +1,22 @@
 
 #include <iostream>
+#include <map>
+#include <set>
 
 using namespace std;
 
 int main(void)
 {
-
+    map<string, size_t> word_count;
+    set<string> exclude {"the", "a", "but"};
+    string word;
+    while(cin >> word)
+        if(exclude.find(word) == exclude.end())
+            ++word_count[word];
+    
+    for(const auto &w : word_count)
+        cout << w.first << " occurs "  << w.second
+             << ((w.second > 1) ? " times " : " time") << endl;
     return 0;
 }
 
