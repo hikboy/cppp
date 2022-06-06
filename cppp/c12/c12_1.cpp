f2e4cead618b707d6472b21610804b32ff4201d8
ljb <hikboy@outlook.com>
2022-06-06 14:34:26 +0800

    test unique_ptr and shared_ptr

--- cppp/c12/c12_1.cpp
+++ cppp/c12/c12_1.cpp
@@ -8,6 +8,11 @@
 
 using namespace std;
 
+void process(shared_ptr<int> ptr)
+{
+
+}
+
 int main(void)
 {
     shared_ptr<string> p1;
@@ -51,6 +56,35 @@ int main(void)
     delete pq;
     pq = nullptr;
 
+    shared_ptr<double> p1l;
+    shared_ptr<int> p2l(new int(42));
+    //shared_ptr<int> p3l = new int(1024);
 
+    if(!p2l.unique())
+        p2l.reset(new int(1000));
+    *p2l += 18;
+    
+    shared_ptr<int> p(new int(42));
+    process(p);
+    process(shared_ptr<int>(p));
+    int i = *p;
+
+    auto ap = new int();
+    auto asp = make_shared<int>();
+    //process(ap);
+    //process(new int());
+    process(asp);
+    process(shared_ptr<int>(p));
+
+    unique_ptr<double> p1a;
+    unique_ptr<int> p2a(new int(42));
+    //unique_ptr<double> p3a(p1a);
+    unique_ptr<double> p4a;
+    //p4a = p1a.release();
+    unique_ptr<double> p5a(p1a.release()); 
+    p5a.reset(p4a.release()); 
+
+    auto ppp = p5a.release();
+    
     return 0;
 }
